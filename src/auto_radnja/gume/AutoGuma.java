781c0c2350b9eae165c8f36c64137ed9b8f6547c
DANKoOoO <danko.97@hotmail.com>
2021-03-17 12:29:54 +0100

    Napisana dokumentacija za klasu AutoGuma.

--- src/auto_radnja/gume/AutoGuma.java
+++ src/auto_radnja/gume/AutoGuma.java
@@ -1,14 +1,46 @@
 package auto_radnja.gume;
-
+/**
+ * Klasas koja predstavlja automobilske gume.
+ * 
+ * AutoGuma ima privatni atribut markaModela tipa String i
+ * 	atribute: precnik, sirina i visina koji su tipa integer.
+ *  
+ * @author danko
+ *
+ */
 public class AutoGuma {
+	/**
+	 * Marka i model gume kao String.
+	 */
 	private String markaModel = null;
+	/**
+	 * Precik gume kao integer.
+	 */
 	private int precnik = -1;
+	/**
+	 * Sirina gume kao integer.
+	 */
 	private int sirina = -1;
+	/**
+	 * Visina gume kao integer.
+	 */
 	private int visina = -1;
 
+	/**
+	 * Prazan konstruktor.
+	 */
 	public AutoGuma() {
 	}
 
+	/**
+	 * Konstruktor koji inicijalizuje objekat i postavlja vrednosti za marku i model gume,
+	 * 	precnik gume, sirinu gume i visinu gume.
+	 * 
+	 * @param markaModel Marka i model gume kao String
+	 * @param precnik Precnik gume kao integer
+	 * @param sirina Sirina gume kao integer
+	 * @param visina Visina gume kao integer
+	 */
 	public AutoGuma(String markaModel, int precnik, int sirina, int visina) {
 		super();
 		setMarkaModel(markaModel);
@@ -16,11 +48,24 @@ public class AutoGuma {
 		setSirina(sirina);
 		setVisina(visina);
 	}
-
+	
+	/**
+	 * Vraca marku i model gume.
+	 * 
+	 * @return Vraca marku i model gume kao String
+	 */
 	public String getMarkaModel() {
 		return markaModel;
 	}
-
+	
+	/**
+	 * Postavlja marku i model gume na novu vrednost.
+	 * 
+	 * @param markaModel Marka i model gume kao String
+	 * 
+	 * @throws java.lang.NullPointerException Ako je uneta vrednost za marku i model null vrednost
+	 * @throws java.lang.RuntimeException Ako uneta vrednost za marku i model ima manje od 3 znaka
+	 */
 	public void setMarkaModel(String markaModel) {
 		if (markaModel == null)
 			throw new NullPointerException("Morate uneti marku i model");
@@ -29,42 +74,92 @@ public class AutoGuma {
 		this.markaModel = markaModel;
 	}
 
+	/**
+	 * Vraca precnik gume.
+	 * 
+	 * @return Vraca precnik gume kao integer
+	 */
 	public int getPrecnik() {
 		return precnik;
 	}
 
+	/**
+	 * Postavlja precnik gume na novu vrednost.
+	 * 
+	 * @param precnik Precnik gume kao integer
+	 * 
+	 * @throws java.lang.RuntimeException Ako je uneta vrednost za precnik manja od 13 i veca od 22 (ovaj if nikada nece
+	 * 	biti ispunjen... verovatno je namenska greska koja ce biti ispravljena testiranjem)
+	 */
 	public void setPrecnik(int precnik) {
 		if (precnik < 13 && precnik > 22)
 			throw new RuntimeException("Precnik van opsega");
 		this.precnik = precnik;
 	}
-
+	
+	/**
+	 * Vraca sirinu gume.
+	 * 
+	 * @return Vraca sirinu gume kao integer.
+	 */
 	public int getSirina() {
 		return sirina;
 	}
 
+	/**
+	 * Postavlja sirinu gume na novu vrednost.
+	 * 
+	 * @param sirina Sirina gume kao integer
+	 * 
+	 * @throws java.lang.RuntimeException Ako je uneta vrednost za sirinu manja od 135 i veca od 355 (ista greska kao za precnik)
+	 */
 	public void setSirina(int sirina) {
 		if (sirina < 135 && sirina > 355)
 			throw new RuntimeException("Sirina van opsega");
 		this.sirina = sirina;
 	}
 
+	/**
+	 * Vraca visinu gume.
+	 * 
+	 * @return Vraca visinu gume kao integer
+	 */
 	public int getVisina() {
 		return visina;
 	}
 
+	/**
+	 * Postavlja visinu gume na novu vrednost.
+	 * 
+	 * @param visina Visina gume kao integer
+	 * 
+	 * @throws java.lang.RuntimeException Ako je uneta vrednost za visinu manja od 25 ili veca od 95
+	 */
 	public void setVisina(int visina) {
 		if (visina < 25 || visina > 95)
 			throw new RuntimeException("Visina van opsega");
 		this.visina = visina;
 	}
-
+	
+	/**
+	 * @return Vraca String sa svim podacima o gumi
+	 */
 	@Override
 	public String toString() {
 		return "AutoGuma [markaModel=" + markaModel + ", precnik=" + precnik + ", sirina=" + sirina + ", visina="
 				+ visina + "]";
 	}
-
+	
+	/**
+	 * Poredi 2 gume i vraca true ako su iste ili false ako nisu.
+	 * Gume se porede po svim parametrima (marka i model, precnik, sirina, visina).
+	 * 
+	 * @return
+	 * <ul>
+	 * 		<li>true ako su oba objekta klase AutoGuma i imaju iste vrednosti za marku i model, precnik, sirinu i visinu </li>
+	 * 		<li>false u svim ostalim slucajevima </li>
+	 * </ul>
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
